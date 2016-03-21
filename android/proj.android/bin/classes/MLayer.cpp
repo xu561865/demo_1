5c713148df78bb0a4e0240657b460324b3ba0a83
xu561865 <xuyi561865@sina.com>
2016-03-21 15:34:18 +0800

    modify

--- android/proj.android/bin/classes/MLayer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MLayer.cpp
-//  legend
-//
-//  Created by Xu Jinyang on 4/12/13.
-//
-//
-
-#include "MLayer.h"
--- android/proj.android/bin/classes/MWebViewImpl.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MWebViewImpl.cpp
-//  legend
-//
-//  Created by xiaofuan on 14-3-5.
-//
-//
-
-#include "MWebViewImpl.h"
