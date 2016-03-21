470805c0dde848038cd2bbcb6cf2c30c9a63b7d5
xu561865 <xuyi561865@sina.com>
2016-03-21 12:45:55 +0800

    delete project/tsDemo/proj.android

--- project/tsDemo/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/skillList.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/skillList.o: \
- jni/../../Classes/skillList.cpp jni/../../Classes/skillList.h \
- /Users/xuyi/program/android/adt-bundle/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/iostream
-
-jni/../../Classes/skillList.h:
-
-/Users/xuyi/program/android/adt-bundle/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/iostream:
