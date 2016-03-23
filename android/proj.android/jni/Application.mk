be435c289db9f58b8abfee8e53ad08cfdb170b82
xu561865 <xuyi561865@sina.com>
2016-03-23 23:46:09 +0800

    添加 支持异常处理

--- android/proj.android/jni/Application.mk
+++ android/proj.android/jni/Application.mk
@@ -1,6 +1,7 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCOCOS2D_DEBUG=1 -std=c++11 -pthread -DLOBR
 APP_CPPFLAGS += -Wno-error=format-security
+APP_CPPFLAGS += -fexceptions
 
 APP_ABI=armeabi
 
