cace95f7c5d2e56b80dfeb44ad31f8d475df8c0a
xu561865 <xuyi561865@sina.com>
2016-04-07 20:46:44 +0800

    add utils files

--- tsDemo/tsDemo/Classes/ui/LayerLogin.cpp
+++ tsDemo/tsDemo/Classes/ui/LayerLogin.cpp
@@ -8,6 +8,7 @@
 #include "SocketManager.h"
 #include "CData.h"
 #include "CustomPop.h"
+#include "utils.h"
 
 SEL_MenuHandler LayerLogin::onResolveCCBCCMenuItemSelector(CCObject * pTarget, const char * pSelectorName)
 {
@@ -125,38 +126,13 @@ void LayerLogin::menuItemCallbackStart(CCObject *pSender)
 
 void LayerLogin::menuItemCallbackLogin(CCObject* pSender)
 {
-    if(strcmp(editBoxUsername->getText(), "")&&strcmp(editBoxPassword->getText(), ""))
+    if(strcmp(editBoxUsername->getText(), "") && strcmp(editBoxPassword->getText(), ""))
     {
         const char * userName=editBoxUsername->getText();
         const char * password=editBoxPassword->getText();
-        int i=0,j=0,uIllegal=0,pIllegal=0;
-        while((userName[i]!='\0'))
-        {
-            if((userName[i]>='0'&&userName[i]<='9')||(userName[i]>='a'&&userName[i]<='z')||(userName[i]>='A'&&userName[i]<='Z'))
-            {
-                i++;
-                continue;
-            }
-            else
-            {
-                i++;
-                uIllegal++;
-            }
-        }
-        while(password[j]!='\0')
-        {
-            if((password[j]>='0'&&password[j]<='9')||(password[j]>='a'&&password[j]<='z')||(password[j]>='A'&&password[j]<='Z'))
-            {
-                j++;
-                continue;
-            }
-            else
-            {
-                j++;
-                pIllegal++;
-            }
-        }
-        if(uIllegal>0||pIllegal>0)
+        
+        
+        if(!utils::checkUserName(userName)||!utils::checkPwd(password))
         {
             CustomPop::show("用户名或密码包含非法字符!~");
         }
@@ -183,7 +159,6 @@ void LayerLogin::menuItemCallbackLogin(CCObject* pSender)
             });
             req->isBackground() = true;
             
-            CCLog("req0: %x", req);
             req->send();
         }
         
